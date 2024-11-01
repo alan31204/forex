6f41a23d7dba06278dfe109b0df53c3688e7d973
Alan Chiu <alanchiu@Alans-MacBook-Air.local>
2024-11-01 00:49:03 -0700

    Initial approach to send HTTP request to API

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameDummy.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameDummy.scala
@@ -6,10 +6,35 @@ import cats.syntax.applicative._
 import cats.syntax.either._
 import forex.domain.{ Price, Rate, Timestamp }
 import forex.services.rates.errors._
+import java.net.HttpURLConnection
+import java.net.URL
+
 
 class OneFrameDummy[F[_]: Applicative] extends Algebra[F] {
 
-  override def get(pair: Rate.Pair): F[Error Either Rate] =
-    Rate(pair, Price(BigDecimal(100)), Timestamp.now).asRight[Error].pure[F]
+  private val endpoint = "http://localhost:8080/rates"
+  private val token = "10dc303535874aeccc86a8251e6992f5"
+  override def get(pair: Rate.Pair): F[Error Either Rate] = {
+    // Rate(pair, Price(BigDecimal(100)), Timestamp.now).asRight[Error].pure[F]
+    // setup the query endpoint
+    val query = s"$endpoint?pair=${pair.from}${pair.to}"
 
+    // try opening up connection and set token 
+    // still need to do cache + find the right answer to return
+    // some easy test cases
+    val connection = new URL(query).openConnection().asInstanceOf[HttpURLConnection]
+    connection.setRequestProperty("token", token)
+
+    val search = 
+      if (connection.getResponseCode == 200) Rate(pair, Price(BigDecimal(100)), Timestamp.now).asRight[Error]
+      else Error.OneFrameLookupFailed("Your query request failed").asLeft[Rate]
+    
+    search.pure[F]
+  }
+    
+  
+  
+
+  
+    
 }
