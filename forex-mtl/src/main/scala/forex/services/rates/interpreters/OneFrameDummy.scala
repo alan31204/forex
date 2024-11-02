c9e7f00d54f688a75a4762eb4a49314bf5f12fb3
Alan Chiu <alanchiu@Alans-MacBook-Air.local>
2024-11-02 16:21:45 -0700

    parse response to rate without caching

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameDummy.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameDummy.scala
@@ -6,28 +6,54 @@ import cats.syntax.applicative._
 import cats.syntax.either._
 import forex.domain.{ Price, Rate, Timestamp }
 import forex.services.rates.errors._
-import java.net.HttpURLConnection
-import java.net.URL
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.net.URI
+import java.time.OffsetDateTime
+import io.circe.parser.parse
+import io.circe.Json
 
 
 class OneFrameDummy[F[_]: Applicative] extends Algebra[F] {
 
+  private val httpClient = HttpClient.newHttpClient()
   private val endpoint = "http://localhost:8080/rates"
   private val token = "10dc303535874aeccc86a8251e6992f5"
+
+  def extractRate(json: Json, pair: Rate.Pair):  Either[Error, Rate] ={
+    val input = for{
+      price <- json.hcursor.get[BigDecimal]("price").toOption
+      // if matched, then it will be BigDecimal, else None
+      timestampString <- json.hcursor.get[String]("timestamp").toOption
+      timestamp = Timestamp(OffsetDateTime.parse(timestampString))
+    } yield Rate(pair, Price(price), timestamp)
+
+    input.toRight(Error.OneFrameLookupFailed("Your parse failed"))
+  }
+
   override def get(pair: Rate.Pair): F[Error Either Rate] = {
     // Rate(pair, Price(BigDecimal(100)), Timestamp.now).asRight[Error].pure[F]
-    // setup the query endpoint
-    val query = s"$endpoint?pair=${pair.from}${pair.to}"
-
-    // try opening up connection and set token 
     // still need to do cache + find the right answer to return
-    // some easy test cases
-    val connection = new URL(query).openConnection().asInstanceOf[HttpURLConnection]
-    connection.setRequestProperty("token", token)
+    val query = s"$endpoint?pair=${pair.from}${pair.to}"
+    val uri = URI.create(query)
+    // try opening up connection and set token 
+    val request = HttpRequest.newBuilder().uri(uri).header("token", token).GET().build()
 
+    val httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
+
+    // How to retrieve answer from connection response 
+    // Store value + compare with cache
     val search = 
-      if (connection.getResponseCode == 200) Rate(pair, Price(BigDecimal(100)), Timestamp.now).asRight[Error]
-      else Error.OneFrameLookupFailed("Your query request failed").asLeft[Rate]
+      if (httpResponse.statusCode() == 200){
+        parse(httpResponse.body()) match {
+          case Right(json) => extractRate(json, pair) 
+          // If pattern matched, use extractRate to get Rate
+          case Left(_) => Left(Error.OneFrameLookupFailed("JSON parse failed"))
+        }
+      }else {
+        Error.OneFrameLookupFailed("Your query request failed").asLeft[Rate]
+      }
     
     search.pure[F]
   }
