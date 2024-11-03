b0b9d085656b0e075f94eb19f14c21441c259eca
Alan Chiu <alanchiu@Alans-MacBook-Air.local>
2024-11-02 21:36:01 -0700

    prototype of cache + calling cache when miss

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameDummy.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameDummy.scala
@@ -13,6 +13,7 @@ import java.net.URI
 import java.time.OffsetDateTime
 import io.circe.parser.parse
 import io.circe.Json
+import scala.collection.mutable
 
 
 class OneFrameDummy[F[_]: Applicative] extends Algebra[F] {
@@ -20,6 +21,30 @@ class OneFrameDummy[F[_]: Applicative] extends Algebra[F] {
   private val httpClient = HttpClient.newHttpClient()
   private val endpoint = "http://localhost:8080/rates"
   private val token = "10dc303535874aeccc86a8251e6992f5"
+  
+  // Initialize cache using mutable.Map -> key being pair, value being Rate
+  private val cache = mutable.Map.empty[Rate.Pair, Rate]
+
+  
+  def getCache(pair: Rate.Pair): Option[Rate]= {
+    // choosing flatMap here allows else case to simply be None instead of Some[None]
+    cache.get(pair).flatMap { 
+    rate =>
+      val checkTime = Timestamp(OffsetDateTime.now.minusMinutes(5))
+      if(rate.timestamp.value.isAfter(checkTime.value)){
+        Some(rate)
+      }
+      else {
+        cache.remove(pair)
+        None
+      }
+    }
+  }
+  
+  def cacheRate(pair: Rate.Pair, rate: Rate): Rate = {
+    cache.put(pair, rate)
+    rate // Return rate
+  }
 
   def extractRate(json: Json, pair: Rate.Pair):  Either[Error, Rate] ={
     val input = for{
@@ -33,34 +58,29 @@ class OneFrameDummy[F[_]: Applicative] extends Algebra[F] {
   }
 
   override def get(pair: Rate.Pair): F[Error Either Rate] = {
-    // Rate(pair, Price(BigDecimal(100)), Timestamp.now).asRight[Error].pure[F]
-    // still need to do cache + find the right answer to return
-    val query = s"$endpoint?pair=${pair.from}${pair.to}"
-    val uri = URI.create(query)
-    // try opening up connection and set token 
-    val request = HttpRequest.newBuilder().uri(uri).header("token", token).GET().build()
+    getCache(pair) match{
+      case Some(rate) => (Right(rate): Either[Error, Rate]).pure[F]
+      // match has two cases from getCache, if found, then rate can be returned
+      case None => 
+        val query = s"$endpoint?pair=${pair.from}${pair.to}"
+        val uri = URI.create(query)
+        // try opening up connection and set token 
+        val request = HttpRequest.newBuilder().uri(uri).header("token", token).GET().build()
+        val httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
 
-    val httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
-
-    // How to retrieve answer from connection response 
-    // Store value + compare with cache
-    val search = 
-      if (httpResponse.statusCode() == 200){
-        parse(httpResponse.body()) match {
-          case Right(json) => extractRate(json, pair) 
-          // If pattern matched, use extractRate to get Rate
-          case Left(_) => Left(Error.OneFrameLookupFailed("JSON parse failed"))
-        }
-      }else {
-        Error.OneFrameLookupFailed("Your query request failed").asLeft[Rate]
-      }
+        val search = 
+          if (httpResponse.statusCode() == 200){
+            parse(httpResponse.body()) match {
+              case Right(json) => extractRate(json, pair).map(rate => cacheRate(pair, rate))
+              // If pattern matched, use extractRate to get Rate
+              case Left(_) => Left(Error.OneFrameLookupFailed("Fail to parse your JSON"))
+            }
+          }else {
+            Error.OneFrameLookupFailed("Your query request failed").asLeft[Rate]
+          }
+        search.pure[F]      
+    }
     
-    search.pure[F]
   }
-    
-  
-  
-
-  
     
 }
