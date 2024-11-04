2847f7e339c246dbdc3cdc6d763dc4a1ff3a00a6
Alan Chiu <alanchiu@Alans-MacBook-Air.local>
2024-11-03 16:18:39 -0800

    update interpreters commit

--- forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
+++ forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
@@ -3,6 +3,11 @@ package forex.services.rates
 import cats.Applicative
 import interpreters._
 
+// If want to provide scalability, we can use cache to support no longer than 5 minutes request
+  // In that, we can avoid keep calling OneFrameAPI
+  // Rates is predefined and use http get response to consume OneFrameAPI
 object Interpreters {
+  
   def dummy[F[_]: Applicative]: Algebra[F] = new OneFrameDummy[F]()
+
 }
