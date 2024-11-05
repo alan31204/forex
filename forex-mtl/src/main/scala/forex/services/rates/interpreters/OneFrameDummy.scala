94f3347be753667ce63f9b83c119986d0002b49d
Alan Chiu <alanchiu@Alans-MacBook-Air.local>
2024-11-04 23:28:32 -0800

    define all pair combinations + newExtractRate for extracting all rates from a single json array

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameDummy.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameDummy.scala
@@ -4,7 +4,7 @@ import forex.services.rates.Algebra
 import cats.Applicative
 import cats.syntax.applicative._
 import cats.syntax.either._
-import forex.domain.{ Price, Rate, Timestamp }
+import forex.domain.{ Price, Rate, Timestamp, Currency}
 import forex.services.rates.errors._
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -24,7 +24,12 @@ class OneFrameDummy[F[_]: Applicative] extends Algebra[F] {
   
   // Initialize cache using mutable.Map -> key being pair, value being Rate
   private val cache = mutable.Map.empty[Rate.Pair, Rate]
+  val allCurrencies = List(Currency.AUD, Currency.CAD, Currency.CHF, Currency.EUR, Currency.GBP, Currency.NZD, Currency.JPY, Currency.SGD, Currency.USD)
 
+  private val allPairs = for{
+    c1 <- allCurrencies
+    c2 <- allCurrencies if c1 != c2
+  } yield Rate.Pair(c1, c2)
   
   def getCache(pair: Rate.Pair): Option[Rate]= {
     // choosing flatMap here allows else case to simply be None instead of Some[None]
@@ -53,11 +58,29 @@ class OneFrameDummy[F[_]: Applicative] extends Algebra[F] {
       price <- firstRateObject.hcursor.get[BigDecimal]("price").toOption
       // if matched, then it will be BigDecimal, else None
       timestampString <- firstRateObject.hcursor.get[String]("time_stamp").toOption
-      // println(timestampString)
       timestamp = Timestamp(OffsetDateTime.parse(timestampString))
       
     } yield Rate(pair, Price(price), timestamp)
     input.toRight(Error.OneFrameLookupFailed("Your parse failed"))
+  }
+
+  def newExtractRate(json: Json):  Either[Error, List[Rate]] ={
+    json.asArray.map{ ratesArray => 
+      ratesArray.flatMap{ singleRate => 
+        for{
+          from <- singleRate.hcursor.get[String]("from").toOption
+          to <- singleRate.hcursor.get[String]("to").toOption
+          price <- singleRate.hcursor.get[BigDecimal]("price").toOption
+          // if matched, then it will be BigDecimal, else None
+          timestampString <- singleRate.hcursor.get[String]("time_stamp").toOption
+          timestamp = Timestamp(OffsetDateTime.parse(timestampString))
+        } yield{
+          val currentPair = Rate.Pair(Currency.fromString(from), Currency.fromString(to))
+          val currentRate = Rate(currentPair, Price(price), timestamp)
+          cacheRate(currentPair, currentRate)
+        }
+      }.toList // Right: List[Rate]
+    }.toRight(Error.OneFrameLookupFailed("At least one of parsing failed"))
   }
 
   override def get(pair: Rate.Pair): F[Error Either Rate] = {
